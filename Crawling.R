810d0e1eb51a01963f5f11b572caafa42515798e
yerimchoi <49264326+yerimchoi@users.noreply.github.com>
2020-08-05 16:54:20 +0900

    Update Crawling.R
    Crawling code of KPI for prices information

--- Crawling.R
+++ Crawling.R
@@ -7,9 +7,11 @@ sapply(pkgs, require, character.only = T)
 # 아래 local chrome 코드에서 에러가 뜨면:
 # 1. 다음 사이트 참조하여 chromedocker, geckodocker 및 selenium 설치, 한 폴더 안에 생성
 #    https://sancj.tistory.com/62
+
 # 2. cmd 관리자 모드로 실행, 다음 코드 입력 (selenium-server-standalone 버전 확인 필요)
 #    cd C:\r_selenium
 #    java -Dwebdriver.gecko.driver="geckodriver.exe" -jar selenium-server-standalone-3.141.59.jar -port 4445
+
 # 3. cmd 창을 아래로 내려놓고 다시 코드 실행
 
 remDr <- remoteDriver(remoteServerAddr = "localhost", 
@@ -24,8 +26,8 @@ txt_id <- remDr$findElement(using="name", value="user_id")
 txt_pw <- remDr$findElement(using="name", value="user_pw")
 login_btn <- remDr$findElement(using="xpath", value="//*[@id=\"mem_info_out\"]/input[3]")
 
-txt_id$setElementAttribute("value", "shinsung3")
-txt_pw$setElementAttribute("value", "rnao3xla")
+txt_id$setElementAttribute("value", "***")
+txt_pw$setElementAttribute("value", "***")
 login_btn$clickElement()
 
 # 한글 페이지 오류 -> 로케일 언어를 영어로 설정
